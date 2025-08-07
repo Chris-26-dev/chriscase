6a88996cba0e5e0b624777b0766a80cfa43c853e
Christian <Christian.m.panerio@gmail.com>
2025-08-07 11:36:45 +0800

    03: animated landing page section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+import { Icons } from "@/components/Icons";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Phone from "@/components/Phone";
+import { Reviews } from "@/components/Reviews";
 import { Check, Star } from "lucide-react";
 import Image from "next/image";
 
@@ -7,13 +9,13 @@ export default function Home() {
   return (
     <div className='bg-slate-50 grainy-light'>
       <section>
-        <MaxWidthWrapper className='pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52'>
+        <MaxWidthWrapper className='pb-24 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-12 xl:pt-12 lg:pb-52'>
           <div className='col-span-2 px-6 lg:px-0 lg:pt-4'>
             <div className='relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start'>
-              <div className='absolute w-28 left-0 -top-20 hidden lg:block'>
+              <div className='absolute w-28 left-0 hidden lg:block'>
                 {/* i forgot this div right here in the video, it's purely visual gradient and looks nice */}
-                <div className='absolute inset-x-0 bottom-0 bg-gradient-to-t via-slate-50/50 from-slate-50 h-28' />
-                <img src='/snake-1.png' className='w-full' />
+                <div className='absolute inset-x-0 bottom-0 bg-gradient-to-t via-slate-50/70 from-slate-50 h-28' />
+                <img src='/i3.png' className='w-full' />
               </div>
               <h1 className='relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl'>
                 Your Image on a{' '}
@@ -75,11 +77,11 @@ export default function Home() {
 
                 <div className='flex flex-col justify-between items-center sm:items-start'>
                   <div className='flex gap-0.5'>
-                    <Star className='h-4 w-4 text-green-600 fill-green-600' />
-                    <Star className='h-4 w-4 text-green-600 fill-green-600' />
-                    <Star className='h-4 w-4 text-green-600 fill-green-600' />
-                    <Star className='h-4 w-4 text-green-600 fill-green-600' />
-                    <Star className='h-4 w-4 text-green-600 fill-green-600' />
+                    <Star className='h-4 w-4 text-yellow-600 fill-yellow-600' />
+                    <Star className='h-4 w-4 text-yellow-600 fill-yellow-600' />
+                    <Star className='h-4 w-4 text-yellow-600 fill-yellow-600' />
+                    <Star className='h-4 w-4 text-yellow-600 fill-yellow-600' />
+                    <Star className='h-4 w-4 text-yellow-600 fill-yellow-600' />
                   </div>
 
                   <p>
@@ -105,6 +107,103 @@ export default function Home() {
           </div>
         </MaxWidthWrapper>
       </section>
+
+      {/* value proposition section */}
+      <section className='bg-slate-100 grainy-dark py-24'>
+        <MaxWidthWrapper className='flex flex-col items-center gap-16 sm:gap-32'>
+          <div className='flex flex-col lg:flex-row items-center gap-4 sm:gap-6'>
+            <h2 className='order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900'>
+              What our{' '}
+              <span className='relative px-2'>
+                customers{' '}
+                <Icons.underline className='hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-black' />
+              </span>{' '}
+              say
+            </h2>
+            <img src='/i1.png' className='w-24 order-0 lg:order-2' />
+          </div>
+
+          <div className='mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16'>
+            {/* first user review */}
+            <div className='flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20'>
+              <div className='flex gap-0.5 mb-2'>
+                <Star className='h-5 w-5 text-yellow-600 fill-yellow-600' />
+                <Star className='h-5 w-5 text-yellow-600 fill-yellow-600' />
+                <Star className='h-5 w-5 text-yellow-600 fill-yellow-600' />
+                <Star className='h-5 w-5 text-yellow-600 fill-yellow-600' />
+                <Star className='h-5 w-5 text-yellow-600 fill-yellow-600' />
+              </div>
+              <div className='text-lg leading-8'>
+                <p>
+                  "The case feels durable and I even got a compliment on the
+                  design. Had the case for two and a half months now and{' '}
+                  <span className='p-0.5 bg-slate-800 text-white'>
+                    the image is super clear
+                  </span>
+                  , on the case I had before, the image started fading into
+                  yellow-ish color after a couple weeks. Love it."
+                </p>
+              </div>
+              <div className='flex gap-4 mt-2'>
+                <img
+                  className='rounded-full h-12 w-12 object-cover'
+                  src='/users/user-4.jpg'
+                  alt='user'
+                />
+                <div className='flex flex-col'>
+                  <p className='font-semibold'>Jonathan</p>
+                  <div className='flex gap-1.5 items-center text-zinc-600'>
+                    <Check className='h-4 w-4 stroke-[3px] text-green-600' />
+                    <p className='text-sm'>Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* second user review */}
+            <div className='flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20'>
+              <div className='flex gap-0.5 mb-2'>
+                <Star className='h-5 w-5 text-yellow-600 fill-yellow-600' />
+                <Star className='h-5 w-5 text-yellow-600 fill-yellow-600' />
+                <Star className='h-5 w-5 text-yellow-600 fill-yellow-600' />
+                <Star className='h-5 w-5 text-yellow-600 fill-yellow-600' />
+                <Star className='h-5 w-5 text-yellow-600 fill-yellow-600' />
+              </div>
+              <div className='text-lg leading-8'>
+                <p>
+                  "I usually keep my phone together with my keys in my pocket
+                  and that led to some pretty heavy scratchmarks on all of my
+                  last phone cases. This one, besides a barely noticeable
+                  scratch on the corner,{' '}
+                  <span className='p-0.5 bg-slate-800 text-white'>
+                    looks brand new after about half a year
+                  </span>
+                  . I dig it."
+                </p>
+              </div>
+              <div className='flex gap-4 mt-2'>
+                <img
+                  className='rounded-full h-12 w-12 object-cover'
+                  src='/users/user-5.jpg'
+                  alt='user'
+                />
+                <div className='flex flex-col'>
+                  <p className='font-semibold'>Josh</p>
+                  <div className='flex gap-1.5 items-center text-zinc-600'>
+                    <Check className='h-4 w-4 stroke-[3px] text-green-600' />
+                    <p className='text-sm'>Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+
+        <div className='pt-16'>
+          <Reviews />
+        </div>
+      </section>
+
     </div>
   );
 }
